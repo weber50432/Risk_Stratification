eb70e1627382f90ba18de914fc3958dd0c6e8d61
weber <weber50432@gmail.com>
2023-08-30 18:11:54 +0800

    add component and set global style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {ContactUsComponent} from './pages/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: SingleCategoryComponent },
+  { path: 'post', component: SinglePostComponent },
+  { path: 'contact', component: ContactUsComponent },
+  {path: 'terms-conditions', component: TermsAndConditionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
